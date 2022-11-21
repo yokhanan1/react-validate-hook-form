735ccd7099ee6cdeb2f2dcc4c5f9a4d48b57bd46
Yokhanan Olotu <yokhanan1@gmail.com>
2022-11-21 09:39:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+
+        <h2 className='main-header'>
+        Form Validation in React Forms using React Hook Form
+        </h2>
+
+        <div>
+
+        </div>
+
+      </div>
+  );
+}
+
+export default App;
